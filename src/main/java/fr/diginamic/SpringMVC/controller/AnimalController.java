4bee02485ea81ae820751bf746fedbbd691d0663
florian <furnari.florian@hotmail.fr>
2023-11-09 16:10:13 +0100

    mise en place de post get pour animal

--- src/main/java/fr/diginamic/SpringMVC/controller/AnimalController.java
+++ src/main/java/fr/diginamic/SpringMVC/controller/AnimalController.java
@@ -4,40 +4,66 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import fr.diginamic.SpringMVC.model.Animal;
+import fr.diginamic.SpringMVC.model.Species;
 import fr.diginamic.SpringMVC.repository.AnimalRepository;
+import fr.diginamic.SpringMVC.repository.SpeciesRepository;
 
+import org.springframework.web.bind.annotation.PostMapping;
 
-@RestController
+@Controller
 @RequestMapping("/animal")
 public class AnimalController {
     @Autowired
     private AnimalRepository animalRepository;
+    @Autowired
+    private SpeciesRepository speciesRepository;
 
     @GetMapping
-    public List<Animal> getAllSpecies() {
-        return animalRepository.findAll();
+    public String getAllSpecies(Model model) {
+        List<Animal> animal = animalRepository.findAll();
+        model.addAttribute("animal", animal);
+        return "animal/animalVue";
+
     }
 
     @GetMapping("/{id}")
     public String initUpdate(@PathVariable("id") Integer id, Model model) {
         Optional<Animal> animal = animalRepository.findById(id);
+        List<Species> speciesList = speciesRepository.findAllOrderedByCommonNameAsc();
+        model.addAttribute("speciesList", speciesList);
         if (animal.isPresent()) {
-            model.addAttribute(animal.get());
-            return "AnimalVue";
+            model.addAttribute("animal", animal);
+            return "animal/animalVueCreate";
         }
         return "error";
     }
 
     @GetMapping("/create")
     public String initCreate(Model model) {
-        model.addAttribute(new Animal());
-        return "AnimalVue";
+        model.addAttribute("animal", new Animal());
+        List<Species> speciesList = speciesRepository.findAllOrderedByCommonNameAsc();
+        model.addAttribute("speciesList", speciesList);
+        return "animal/animalVueCreate";
     }
+
+    @PostMapping()
+    public String createOrUpdate(Animal animalItem) {
+        this.animalRepository.save(animalItem);
+        return "redirect:/animal";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable("id") Integer animalId) {
+        Optional<Animal> animalToDelete = this.animalRepository.findById(animalId);
+        animalToDelete.ifPresent(animal -> this.animalRepository.delete(animal));
+        return "redirect:/animal";
+    }
+
 }
